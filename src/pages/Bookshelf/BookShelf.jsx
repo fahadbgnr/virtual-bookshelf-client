3df0334d8e681ffb86d83b0a5b815f38ff531d88
fahadbgnr <aminulislamfahad1@gmail.com>
2025-08-10 00:54:42 +0600

    bookshelf section update

--- src/pages/Bookshelf/BookShelf.jsx
+++ src/pages/Bookshelf/BookShelf.jsx
@@ -53,7 +53,6 @@ const BookShelf = () => {
                 </select>
             </div>
 
-
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
                 {filteredBooks.length > 0 ? (
                     filteredBooks.map(book => (
